c8cd6ff49c16c56df7b08b8dfa127b50eed2a9a5
keith <miyamii.x@gmail.com>
2025-04-03 01:58:51 +0800

    fix: changed requirements to args, model loader changed depending on whether whisper or nllb is selected

--- merge-and-upload.py
+++ merge-and-upload.py
@@ -1,5 +1,5 @@
 import argparse
-from transformers import WhisperForConditionalGeneration
+from transformers import WhisperForConditionalGeneration, AutoModelForSeq2SeqLM
 from peft import PeftModel
 from pathlib import Path
 from huggingface_hub import HfApi
@@ -9,13 +9,17 @@ import subprocess
 def main():
     parser = argparse.ArgumentParser(description="Fine-tune and upload model with LoRA adaptation.")
     parser.add_argument("--adapter_path", type=str, required=True, help="Path to the adapter checkpoint.")
-    parser.add_argument("--repo_owner", type=str, required=True, help="Hugging Face repo owner.", default="keeve101")
+    parser.add_argument("--repo_owner", type=str, help="Hugging Face repo owner.", default="keeve101")
     parser.add_argument("--model", type=str, choices=["openai/whisper-large-v3-turbo", "facebook/nllb-200-distilled-600M"], default="openai/whisper-large-v3-turbo", help="Model to use for fine-tuning.")
     parser.add_argument("--convert_to_pt", action="store_true", help="Whether to convert to PT format.")
-    parser.add_argument("--model_name", type=str, help="Custom model name.")
+    parser.add_argument("--model_name", type=str, help="Custom model name.", required=True)
     args = parser.parse_args()
 
-    model = WhisperForConditionalGeneration.from_pretrained(args.model, torch_dtype=torch.float16)
+    if args.model == "openai/whisper-large-v3-turbo":
+        model = WhisperForConditionalGeneration.from_pretrained(args.model, torch_dtype=torch.float16)
+    else:
+        model = AutoModelForSeq2SeqLM.from_pretrained(args.model, torch_dtype=torch.float16)
+
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model.to(device)
 
@@ -28,6 +32,8 @@ def main():
     model_name = args.model_name
 
     repo_id = f"{args.repo_owner}/{model_name}"
+
+    merged_model.push_to_hub(repo_id)
 
     if args.convert_to_pt:
         command = [
