fix: fixed issues with data collator output, edited training params, enable input_grads for encoder embed_token layer for gradient checkpointing

nllb-finetune-lora-balanced-multi-corpora.py
@@ -5,7 +5,7 @@ import evaluate
 import torch
 import numpy as np
 
-from transformers import DataCollatorForSeq2Seq
+from transformers import DataCollatorWithPadding
 from transformers.trainer_utils import get_last_checkpoint
 from datasets import load_dataset, get_dataset_config_names, concatenate_datasets, Dataset
 
@@ -20,10 +20,12 @@ LANGUAGES = {
    'en': "eng_Latn"
 }
 
-class DataCollatorForSeq2SeqRandomizedBidirectional(DataCollatorForSeq2Seq):
-    def __call__(self, features, return_tensors=None):
-        input_ids = []
-        labels = []
+max_length = 512
+
+class DataCollatorForSeq2SeqRandomizedBidirectional(DataCollatorWithPadding):
+    def __call__(self, features, return_tensors="pt"):
+        tokenized_inputs = []
+        tokenized_labels = []
         
         for feature in features:
             src_lang = LANGUAGES['en']
@@ -36,35 +38,52 @@ class DataCollatorForSeq2SeqRandomizedBidirectional(DataCollatorForSeq2Seq):
                 src_lang, tgt_lang = tgt_lang, src_lang
                 src, tgt = tgt, src
 
-            
             # Set the tokenizer's source language dynamically
             self.tokenizer.src_lang = src_lang
-            input_batch = self.tokenizer(
+            tokenized_input = self.tokenizer(
                 src,
-                return_tensors="pt",
-                padding=True,
                 truncation=True,
-                max_length=self.tokenizer.model_max_length,
+                max_length=self.max_length,
             )
             
             self.tokenizer.src_lang = tgt_lang
-            label_batch = self.tokenizer(
+            tokenized_label = self.tokenizer(
                 tgt,
-                return_tensors="pt",
-                padding=True,
                 truncation=True,
-                max_length=self.tokenizer.model_max_length,
+                max_length=self.max_length,
             )
-
-            label_batch.input_ids[label_batch.input_ids == self.tokenizer.pad_token_id] = -100 # ignore pad tokens
-
-            input_ids.append(input_batch['input_ids'])
-            labels.append(label_batch['input_ids'])
             
-        batch = {
-            'input_ids': torch.cat(input_ids, dim=0),
-            'labels': torch.cat(labels, dim=0)
+            tokenized_inputs.append(tokenized_input)
+            tokenized_labels.append(tokenized_label)
+        
+        source_inputs = {
+            "input_ids": [x["input_ids"] for x in tokenized_inputs],
+            "attention_mask": [x["attention_mask"] for x in tokenized_inputs],
         }
+        
+        target_inputs = {
+            "input_ids": [x["input_ids"] for x in tokenized_labels],
+        }
+        
+        batch = self.tokenizer.pad(
+            source_inputs,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors=return_tensors,
+        )
+        
+        labels_batch = self.tokenizer.pad(
+            target_inputs,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors=return_tensors,
+        )
+        
+        labels_batch["input_ids"][labels_batch["input_ids"] == self.tokenizer.pad_token_id] = -100
+        
+        batch["labels"] = labels_batch["input_ids"]
         
         return batch
 
@@ -75,7 +94,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
-data_collator = DataCollatorForSeq2SeqRandomizedBidirectional(tokenizer=tokenizer, model=model)
+data_collator = DataCollatorForSeq2SeqRandomizedBidirectional(tokenizer=tokenizer, max_length=max_length)
 
 lora_config = LoraConfig(
     r=32,
@@ -83,7 +102,18 @@ lora_config = LoraConfig(
     target_modules=["q_proj", "v_proj"],
     lora_dropout=0.05,
     bias="none",
+    task_type="SEQ_2_SEQ_LM",
 )
+
+
+def make_inputs_require_grad(module, input, output):
+    output.requires_grad_(True)
+    
+model.config.use_cache = False
+
+model.model.encoder.embed_tokens.register_forward_hook(make_inputs_require_grad)
+
+model.gradient_checkpointing_enable({"use_reentrant": False})
 
 model = get_peft_model(model, lora_config)
 
@@ -92,9 +122,9 @@ model.to(device)
 config_names = get_dataset_config_names(dataset_path)
 config_names = [config_name for config_name in config_names if config_name in LANGUAGES.keys()]
 
-dataset: Dataset = concatenate_datasets([load_dataset(dataset_path, config_name, split="train") for config_name in config_names])
+dataset: Dataset = concatenate_datasets([load_dataset(dataset_path, config_name, split="train").filter(lambda x: x["en"] != "" and x["other"] != "") for config_name in config_names])
 
-train_test_split = dataset.train_test_split(test_size=0.2)
+train_test_split = dataset.train_test_split(test_size=0.01)
 
 train_dataset = train_test_split["train"]
 val_dataset = train_test_split["test"]
@@ -102,7 +132,6 @@ val_dataset = train_test_split["test"]
 wer_metric = evaluate.load("wer")
 cer_metric = evaluate.load("cer")
 bleu_metric = evaluate.load("sacrebleu")
-comet_metric = evaluate.load("comet")
     
 def compute_metrics(pred):
     pred_ids = pred.predictions
@@ -124,20 +153,20 @@ def compute_metrics(pred):
 output_dir = "nllb-finetune-lora-balanced-multi-corpora"
 training_args = Seq2SeqTrainingArguments(
     output_dir=output_dir,
-    per_device_train_batch_size=16,
+    per_device_train_batch_size=4,
     gradient_accumulation_steps=2,
     learning_rate=1e-5,
     warmup_steps=500,
-    max_steps=2000,
+    #max_steps=2000,
+    num_train_epochs=1,
     gradient_checkpointing=True,
     fp16=True,
     evaluation_strategy="steps",
-    per_device_eval_batch_size=1,
+    per_device_eval_batch_size=4,
     predict_with_generate=True,
-    generation_max_length=225,
-    save_steps=400,
-    eval_steps=400,
-    logging_steps=100,
+    save_steps=10000,
+    eval_steps=10000,
+    logging_steps=200,
     report_to=["tensorboard"],
     push_to_hub=False,
     remove_unused_columns=False,